1c919ded90474309c7bcc292a326cc43e933ba61
Gary <netzver@gmail.com>
2018-05-14 17:31:16 +0300

    fix design

--- src/components/Header/headerComponent.js
+++ src/components/Header/headerComponent.js
@@ -14,42 +14,39 @@ export default class HeaderComponent extends Component {
 
   render () {
     return (
-      <Navbar className='nav-head' collapseOnSelect fluid>
+      <Navbar className='nav-head header' collapseOnSelect fluid>
         <Navbar.Header>
           <Navbar.Brand>
             <Link to='/' className='logo'>
               {appName}
-              <img src={logo} alt='logo' width="24"/>
+              <img src={logo} alt='logo' width={24} />
             </Link>
           </Navbar.Brand>
-          <Navbar.Toggle/>
+          <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav className='nav-group'>
-            <NavItem href="/">Projects</NavItem>
-            <NavItem href="/demo">Store</NavItem>
-            <NavItem href="/demo">Creators</NavItem>
+            <NavItem href='/'>Projects</NavItem>
+            <NavItem href='/demo'>Store</NavItem>
+            <NavItem href='/demo'>Creators</NavItem>
           </Nav>
           <Nav className='nav-group-right' pullRight>
             {this.props.userName &&
             <NavDropdown
               title={
-                <div className="pull-left">
-                  <img className="user-pic img-circle"
-                       src={userPic}
-                       alt="user pic"
-                  />
+                <div className='pull-left'>
+                  <img className='user-pic img-circle' src={userPic} alt='user pic' />
                   {this.props.userName}
                 </div>
               }
-              id="basic-nav-dropdown">
+              id='basic-nav-dropdown'>
               <MenuItem>My projects</MenuItem>
               <MenuItem>Edit profile</MenuItem>
-              <MenuItem divider/>
+              <MenuItem divider />
               <MenuItem onClick={this.logout}>Logout</MenuItem>
             </NavDropdown>}
-            {!this.props.userName && <NavItem href="/login">Login</NavItem>}
-            {!this.props.userName && <NavItem href="/sign-up">Sign Up</NavItem>}
+            {!this.props.userName && <NavItem href='/login'>Login</NavItem>}
+            {!this.props.userName && <NavItem href='/sign-up'>Sign Up</NavItem>}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
