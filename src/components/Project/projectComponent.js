0ee0da6eab83c44a828cb867fabfd0025fb365cd
Gary <netzver@gmail.com>
2018-05-18 14:33:48 +0300

    fix problem with fetching project

--- src/components/Project/projectComponent.js
+++ src/components/Project/projectComponent.js
@@ -19,29 +19,51 @@ export default class ProjectComponent extends Component {
   }
 
   render () {
-    const {project, loading, error} = this.props
+    const {project} = this.props
 
     return (
       <div className='project-page'>
-        <h1>Project:</h1>
+        {project && (
+          <div>
+            <h1>Project:</h1>
 
-        <Row className='table-head'>
-          <Col xs={0} sm={1} className='table-head-name'/>
-          <Col xs={3} sm={3} className='hidden-sm-down table-head-name'>title</Col>
-          <Col xs={3} sm={2} className='hidden-sm-down table-head-name'>created by</Col>
-          <Col xs={2} sm={2} className='hidden-sm-down table-head-name'>rating<br/><span>price/efficiency</span></Col>
-          <Col xs={2} sm={2} className='hidden-sm-down table-head-name'>details</Col>
-          <Col xs={2} sm={2} className='hidden-sm-down table-head-name'>status</Col>
-        </Row>
+            <Row className='table-head'>
+              <Col xs={0} sm={1} className='table-head-name'/>
+              <Col xs={3} sm={3} className='hidden-sm-down table-head-name'>title</Col>
+              <Col xs={3} sm={2} className='hidden-sm-down table-head-name'>created by</Col>
+              <Col xs={2} sm={2} className='hidden-sm-down table-head-name'>rating<br/><span>price/efficiency</span></Col>
+              <Col xs={2} sm={2} className='hidden-sm-down table-head-name'>details</Col>
+              <Col xs={2} sm={2} className='hidden-sm-down table-head-name'>status</Col>
+            </Row>
 
-        {
-          project.snapshots.map((snapshot, i) =>
-            <Row key={i} className='table-row'>
+            {
+              project.snapshots.map((snapshot, i) =>
+                <Row key={i} className='table-row'>
+                  <Col xs={1} className='table-data icon'>
+                    <i className='fa fa-cubes fa-eye'/>
+                  </Col>
+                  <Col xs={3} className='table-data title'>
+                    <Link to={`/cad/${project._key}/${snapshot._key}`}>{snapshot.title}</Link>
+                    <p>34 minutes ago hardcoded</p>
+                  </Col>
+                  <Col xs={2} className='table-data'>
+                    project - createdBy
+                    <p>Project manager hardcoded</p>
+                  </Col>
+
+                  <Col xs={2} className='table-data'>rating...</Col>
+                  <Col xs={2} className='table-data'>description...</Col>
+                  <Col xs={1} className='table-data'>in progress</Col>
+                </Row>
+              )
+            }
+
+            <Row className='table-row'>
               <Col xs={1} className='table-data icon'>
                 <i className='fa fa-cubes fa-eye'/>
               </Col>
               <Col xs={3} className='table-data title'>
-                <Link to={`/cad/${project._key}/${snapshot._key}`}>{snapshot.title}</Link>
+                <Link to={`/cad/${project._key}`}>{project.title}</Link>
                 <p>34 minutes ago hardcoded</p>
               </Col>
               <Col xs={2} className='table-data'>
@@ -53,26 +75,8 @@ export default class ProjectComponent extends Component {
               <Col xs={2} className='table-data'>description...</Col>
               <Col xs={1} className='table-data'>in progress</Col>
             </Row>
-          )
-        }
-
-        <Row className='table-row'>
-          <Col xs={1} className='table-data icon'>
-            <i className='fa fa-cubes fa-eye'/>
-          </Col>
-          <Col xs={3} className='table-data title'>
-            <Link to={`/cad/${project._key}`}>{project.title}</Link>
-            <p>34 minutes ago hardcoded</p>
-          </Col>
-          <Col xs={2} className='table-data'>
-            project - createdBy
-            <p>Project manager hardcoded</p>
-          </Col>
-
-          <Col xs={2} className='table-data'>rating...</Col>
-          <Col xs={2} className='table-data'>description...</Col>
-          <Col xs={1} className='table-data'>in progress</Col>
-        </Row>
+          </div>
+        )}
       </div>
     )
   }
