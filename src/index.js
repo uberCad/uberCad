3ffa832edc4099f5ecfcd87a08ff3f74088f599b
Volodymyr Shcherbyna <vladimshc@gmail.com>
2018-06-19 23:58:36 +0300

    disable service worker in production. Fix style logo, userName.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import en from 'react-intl/locale-data/en'
 import ru from 'react-intl/locale-data/ru'
 
 import App from './AppContainer'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
+import { unregister } from './registerServiceWorker'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/lib/integration/react'
 import configureStore from './configureStore'
@@ -18,8 +19,9 @@ addLocaleData([...en, ...ru])
 render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <App />
+      <App/>
     </PersistGate>
   </Provider>, document.getElementById('root')
 )
-registerServiceWorker()
+// registerServiceWorker()
+unregister()
